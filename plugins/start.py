3cc43ba45528604a748fa12915ef2c753cf0fb2d
Lk2030 <153378150+Lk2030@users.noreply.github.com>
2023-12-14 13:41:21 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -61,8 +61,9 @@ async def start_command(client: Client, message: Message):
         except:
             await message.reply_text("Something went wrong..!")
             return
-        await asyncio.sleep(10)
         await temp_msg.delete()
+        await asyncio.sleep(10)
+        await messages.delete()
 
         for msg in messages:
 
